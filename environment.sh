a40c8fc634e35d24cce27791df4c33b7ed68a21f
Tony Hsiao <tonycar12002@gmail.com>
2019-02-26 22:28:01 +0800

    update sheel script

--- environment.sh
+++ environment.sh
@@ -1,6 +1,3 @@
 source /opt/ros/kinetic/setup.bash
 source $HOME/ugv/catkin_ws/devel/setup.bash
 
-GAZEBO_MODEL_PATH=$HOME/ugv/catkin_ws/src/husky/husky_gazebo/models
-export GAZEBO_MODEL_PATH
-echo "Set GAZEBO_MODEL_PATH to: $GAZEBO_MODEL_PATH"

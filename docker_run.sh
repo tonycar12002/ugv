e98936364dcf54e8b9e5d579f187eb818e9285b9
Tony Hsiao <tonycar12002@gmail.com>
2019-02-26 15:27:03 +0800

    add r os lib in docker

--- docker_run.sh
+++ docker_run.sh
@@ -25,6 +25,12 @@ echo "Target docker",  $docker_name
 
 if [ "$SAME" == "true" ]; then
 	docker exec -it $docker_name bash
+elif [ "$docker_name" == "subt_gazebo" ]; then
+	nvidia-docker run --name $docker_name  --rm -it --net=host --privileged -v /dev:/dev \
+				-v /etc/localtime:/etc/localtime:ro -v /var/run/docker.sock:/var/run/docker.sock \
+				-v ~/.bashrc:/root/.bashrc \
+				--pid=host \
+				--env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" $docker_target
 else
 	nvidia-docker run --name $docker_name  --rm -it --net=host --privileged -v /dev:/dev \
 				-v /etc/localtime:/etc/localtime:ro -v /var/run/docker.sock:/var/run/docker.sock \

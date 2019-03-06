aac15d1750ca2e6658260a2750ba388c4a1d9f02
Tony Hsiao <tonycar12002@gmail.com>
2019-03-07 00:30:52 +0800

    fix local map drift

--- catkin_ws/src/point_cloud/pointcloud_to_laserscan/src/local_cost2d.py
+++ catkin_ws/src/point_cloud/pointcloud_to_laserscan/src/local_cost2d.py
@@ -7,7 +7,7 @@ from nav_msgs.msg import OccupancyGrid, MapMetaData
 from message_filters import ApproximateTimeSynchronizer, TimeSynchronizer, Subscriber
 import tf
 from math import cos ,sin, pi, sqrt
-
+import math
 class LocalCost2d(object):
     def __init__(self):
 
@@ -22,8 +22,10 @@ class LocalCost2d(object):
         self.map_length   = rospy.get_param("~map_length", 35) 
         self.wait_time  = rospy.get_param("~wait_time", 0.1)
         self.vehicle_size  = rospy.get_param("~vehicle_size", 0.1)
+        self.drift_x = rospy.get_param("~drift_x", 0.1) # case by odom and lidar position
+        self.drift_y = rospy.get_param("~drift_y", 0.1)
 
-        self.cell_length = int(self.map_length/self.cell_size) * 2
+        self.cell_length = int(self.map_length/self.cell_size*2) 
         self.frame_id  = rospy.get_param("~frame_id", "odom")
         self.frame_rate = 0.5
 
@@ -85,48 +87,57 @@ class LocalCost2d(object):
         '''
 
         # initial cell 
-        data = [0] * map_data_info.width * map_data_info.height
+        data = np.zeros(map_data_info.width * map_data_info.height)
+        
         # read laser scan to grid map 
         for laser in range(0, len(self.scan.ranges)):
             scan_range = self.scan.ranges[laser]
-            rad = self.scan.angle_min + laser * self.scan.angle_increment + pi/2  + self.vehicle_yaw 
+            rad = self.scan.angle_min + laser * self.scan.angle_increment + self.vehicle_yaw 
 
             # remove points out of range 
-            if scan_range is None or scan_range < self.scan.range_min or scan_range > self.scan.range_max or scan_range > self.map_length - 5: # 
+            if scan_range is None or scan_range < self.scan.range_min or scan_range > self.scan.range_max or scan_range > self.map_length - 3: # 
                 continue
 
             cell_num = self.get_cell_number(scan_range, rad, map_data_info)
             for value in cell_num:
-                value = map_data_info.width * map_data_info.height - value 
                 data[value] = 100
-
+        
+        #for i in range(0, 200):
+        #    data[i] = -1
         #print("==========================")
         occupancy_grid.data = data
         
         self.pub_grid_map.publish(occupancy_grid)
 
     def get_cell_number(self, scan_range, rad, map_data_info):
-       
+        #print(rad/pi*180%360)
         scan_x = scan_range * cos(rad) 
         scan_y = scan_range * sin(rad) 
 
-        dis_x = self.odom.pose.pose.position.x - map_data_info.origin.position.x
-        dis_y = self.odom.pose.pose.position.y - map_data_info.origin.position.y
+        dis_x = self.odom.pose.pose.position.x - map_data_info.origin.position.x + self.drift_x
+        dis_y = self.odom.pose.pose.position.y - map_data_info.origin.position.y + self.drift_y
 
         scan_x = scan_x + dis_x
         scan_y = scan_y + dis_y
 
         scan_list = []
         cell_list = []
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                scan_list.append( [scan_x+i*self.vehicle_size , scan_y+j*self.vehicle_size ] )
 
+        x_min = scan_x-self.vehicle_size
+        x_max = scan_x+self.vehicle_size
+        y_min = scan_y-self.vehicle_size
+        y_max = scan_y+self.vehicle_size
+
+        scan_list.append([scan_x, scan_y])
+        for i in np.arange(x_min, x_max, self.cell_size):
+            for j in np.arange(y_min, y_max, self.cell_size):
+                scan_list.append( [i, j] )
+        
         for scan in scan_list:
-            cell_y = int(scan[0] / self.cell_size)
-            cell_x = int(scan[1] / self.cell_size)
-            cell_x = map_data_info.width - cell_x
-            data = int(cell_y * map_data_info.width + cell_x)
+            cell_x = int(math.ceil(scan[0] / self.cell_size)) -1
+            cell_y = int(math.ceil(scan[1] / self.cell_size)) -1
+            data = cell_y * map_data_info.width + cell_x
+            #print(cell_x, scan[0])
             cell_list.append(data)
 
         return cell_list
